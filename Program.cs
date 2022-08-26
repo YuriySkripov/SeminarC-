12bcef4019fa3acd5ab21cd144ebc76b014af6c1
YuriySkripov <yuriy_19.90@mail.ru>
2022-08-27 01:54:06 +0700

    добавили файл

---
+++ Program.cs
@@ -0,0 +1,70 @@
+﻿//Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+/*
+int num1;
+Console.Write("Введите число 1: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+int num2;
+Console.Write("Введите число 2: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+if(num1>num2){Console.WriteLine("Наибольшее = " + num1);
+                Console.WriteLine("Наименьшее = " + num2);}
+else{Console.WriteLine("Наибольшее = " + num2);
+        Console.WriteLine("Наименьшее = " + num1);}
+if (num1 == num2)
+{
+    Console.WriteLine("Числа одинаковые");
+}
+*/
+
+//Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+/*
+int num1;
+Console.Write("Введите число 1: ");
+num1 = Convert.ToInt32(Console.ReadLine());
+int num2;
+Console.Write("Введите число 2: ");
+num2 = Convert.ToInt32(Console.ReadLine());
+int num3;
+Console.Write("Введите число 3: ");
+num3 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2) {
+if (num1 > num3) {Console.WriteLine("Наибольшее " + num1);}
+}
+
+if (num2 > num1) {
+if (num2 > num3) {Console.WriteLine("Наибольшее " + num2);}
+}
+
+if (num3 > num1) {
+if (num3 > num2) {Console.WriteLine("Наибольшее " + num3);}
+}
+*/
+
+//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+ /*int num1;
+  Console.Write("Введите число: ");
+  num1 = Convert.ToInt32(Console.ReadLine());
+
+ 
+
+  if (num1 % 2==0) {Console.WriteLine("Число " + num1 + " - четное");}
+  else {Console.WriteLine("Число " + +num1+ " - нечетное");}
+*/
+
+//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+/*
+int num;
+
+Console.Write("Введите день недели: ");
+num = Convert.ToInt32(Console.ReadLine());
+if (num == 1) {Console.WriteLine("Нет");}
+else if (num == 2) {Console.WriteLine("Нет");}
+else if (num == 3) {Console.WriteLine("Нет");}
+else if (num == 4) {Console.WriteLine("Нет");}
+else if (num == 5) {Console.WriteLine("Нет");}
+else if (num == 6) {Console.WriteLine("Да");}
+else if (num == 7) {Console.WriteLine("Да");}
+*/
+
